9c15c7468992f8d74875f8c5be4b1f426409c29d
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-06-27 12:06:41 -0400

    mv jobs and quotes into prefixes

---
+++ app/routes/quotes/home.tsx
@@ -0,0 +1,20 @@
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Quotes • RV" },
+    { name: "description", content: "Quotes for Real Value" },
+  ];
+}
+
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE }
+}
+
+export default function Quotes({ loaderData }: Route.ComponentProps) {
+  return (
+    <div className="w-full h-full p-8">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">Quotes</h1>
+    </div>
+  );
+}
