cd9ce3e96f24b47cc2b10ae1b61841029cf3cd19
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-06-27 12:05:18 -0400

    mv calendar into prefix

---
+++ app/routes/calendar/home.tsx
@@ -0,0 +1,20 @@
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Calendar • RV" },
+    { name: "description", content: "Calendar for Real Value" },
+  ];
+}
+
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE }
+}
+
+export default function Calendar({ loaderData }: Route.ComponentProps) {
+  return (
+    <div className="w-full h-full p-8">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">Calendar</h1>
+    </div>
+  );
+}
