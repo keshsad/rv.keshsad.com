c83b3dc32cd890f030b205fa4183bb89604005bc
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-07-16 16:58:36 -0400

    client, cookies, tokens

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@openauthjs/openauth/client"
-import { createCookie } from "react-router"
+import { createCookie, redirect } from "react-router"
 
-const client = createClient({
+export const client = createClient({
   clientID: "rv-web-client",
   issuer: "https://auth.keshsad.com"
 })
@@ -10,3 +10,31 @@ const client = createClient({
 //
 // loader/action returns `Response` object,
 // add `Set-Cookie` header to tell browser to store auth cookie
+
+const accessTokenCookie = createCookie("access_token", {
+  path: "/",
+  httpOnly: true,
+  sameSite: "lax",
+  maxAge: 60 * 60 * 24
+})
+
+const refreshTokenCookie = createCookie("access_token", {
+  path: "/",
+  httpOnly: true,
+  sameSite: "lax",
+  maxAge: 60 * 60 * 24
+})
+
+export async function setTokens(access: string, refresh: string) {
+  const headers = new Headers()
+  headers.append("Set-Cookies", await accessTokenCookie.serialize(access))
+  headers.append("Set-Cookies", await refreshTokenCookie.serialize(refresh))
+  return redirect("/", { headers })
+}
+
+export async function getTokens(request: Request) {
+  const cookies = request.headers.get("Cookie")
+  const access = (await accessTokenCookie.parse(cookies)) || {}
+  const refresh = (await refreshTokenCookie.parse(cookies)) || {}
+  return { access, refresh }
+}
