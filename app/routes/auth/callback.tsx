1b511144a5e96abba3fc6e2bd672818ec066b259
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-07-16 17:01:43 -0400

    squash! login page, component, and callback

--- app/routes/auth/callback.tsx
+++ app/routes/auth/callback.tsx
@@ -1,2 +1,19 @@
 // loader (runs on server) exchanges code for token
 // and sets the `HttpOnly` cookie
+
+import { redirect, type LoaderFunctionArgs } from "react-router";
+import { client, setTokens } from "~/lib/auth";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url)
+  const code = url.searchParams.get("code")
+  if (!code) throw new Error("Authorization code not found in callback URL.")
+
+  const exchange = await client.exchange(code, `${url.origin}/callback`)
+  if (exchange.err) {
+    console.error("Token exchange error:", exchange.err)
+    return redirect("/login?error=auth_failed")
+  }
+
+  return setTokens(exchange.tokens.access, exchange.tokens.refresh)
+}
